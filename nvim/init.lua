70f47cc0765ade22100bc1e0e70483f83a5da379
chstoubos <chstoubos@gmail.com>
2024-03-09 21:15:01 +0200

    i3 and i3status changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -520,9 +520,6 @@ require('lazy').setup({
       },
       formatters_by_ft = {
         lua = { 'stylua' },
-        cpp = { 'clang-format' },
-        c = { 'clang-format' },
-        rust = { 'rust-analyzer' },
       },
     },
   },
