7b87a57e11969220830b7b9fe29de48ef57b99d0
Chris Stoumpos <chstoubos@gmail.com>
2025-05-18 23:31:00 +0300

    nvim: Update for 0.11

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -6,18 +6,15 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
