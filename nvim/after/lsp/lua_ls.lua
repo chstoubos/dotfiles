0d653e64a0a11832214d090838c8eac78d074ed0
Chris Stoumpos <chstoubos@gmail.com>
2026-02-04 18:24:40 +0200

    nvim: Fix lua_ls package.path

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,8 +1,20 @@
 return {
   settings = {
     Lua = {
-      completion = { callSnippet = 'Replace' },
-      diagnostics = { globals = { 'vim' } },
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      -- Optional but commonly useful for Neovim config editing:
+      workspace = {
+        checkThirdParty = false,
+      },
+      completion = {
+        callSnippet = 'Replace',
+      },
     },
   },
 }
