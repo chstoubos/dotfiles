c6f3d510fc48a461625a412e5849115c3ec2a8c5
Chris Stoumpos <chstoubos@gmail.com>
2026-02-05 12:50:56 +0200

    nvim: format clangd overrides

--- nvim/after/lsp/clangd.lua
+++ nvim/after/lsp/clangd.lua
@@ -1,14 +1,13 @@
-local tc = "/home/chris/etheron/bsp/l4t-gcc/aarch64--glibc--stable-2022.08-1"
+local tc = '/home/chris/etheron/bsp/l4t-gcc/aarch64--glibc--stable-2022.08-1'
 
 return {
   cmd = {
-    "clangd",
-    "--query-driver="
-      .. table.concat({
-        "/usr/bin/c++",
-        "/usr/bin/g++",
-        tc .. "/bin/aarch64*-gcc",
-        tc .. "/bin/aarch64*-g++",
-      }, ","),
+    'clangd',
+    '--query-driver=' .. table.concat({
+      '/usr/bin/c++',
+      '/usr/bin/g++',
+      tc .. '/bin/aarch64*-gcc',
+      tc .. '/bin/aarch64*-g++',
+    }, ','),
   },
 }
