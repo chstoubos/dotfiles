da6a5e0ee0eb84c27f33f792826a956756df076c
Chris Stoumpos <chstoubos@gmail.com>
2026-02-26 01:55:32 +0200

    Add polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar_main.log
+polybar main 2>&1 | tee -a /tmp/polybar_main.log & disown
