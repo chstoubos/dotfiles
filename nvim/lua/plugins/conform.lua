c2400a04ed959d6c41342f837b9d704e4a26dbda
Chris Stoumpos <chstoubos@gmail.com>
2026-02-27 11:41:08 +0200

    nvim: Add range formatting on visual selected text

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -13,6 +13,22 @@ return {
       mode = '',
       desc = '[F]ormat buffer',
     },
+    {
+      '<leader>fs',
+      function()
+        require('conform').format({ async = true, lsp_format = 'fallback' }, function(err)
+          if err then
+            return
+          end
+          local mode = vim.api.nvim_get_mode().mode
+          if vim.startswith(string.lower(mode), 'v') then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+          end
+        end)
+      end,
+      mode = 'v',
+      desc = '[F]ormat [S]election',
+    },
   },
   opts = {
     notify_on_error = false,
