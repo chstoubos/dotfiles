e0ff2c0a4198855f3a6e2e7b320055f80e01a4eb
Chris Stoumpos <chstoubos@gmail.com>
2026-08-25 12:37:05 +0300

    Add C to conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -36,6 +36,7 @@ return {
       lua = { 'stylua' },
       rust = { 'rustfmt', lsp_format = 'fallback' },
       cpp = { 'clang-format', lsp_format = 'first' },
+      c = { 'clang-format', lsp_format = 'first' },
       javascript = { 'prettier' },
       javascriptreact = { 'prettier' },
       typescript = { 'prettier' },
