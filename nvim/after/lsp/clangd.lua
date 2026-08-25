6d53dfcdb93c1eeb74067eff2f9401b7df7b6b31
Chris Stoumpos <chstoubos@gmail.com>
2026-08-25 18:26:43 +0300

    Add esp toolchain to clangd config

--- nvim/after/lsp/clangd.lua
+++ nvim/after/lsp/clangd.lua
@@ -1,4 +1,5 @@
 local tc = '/home/chris/etheron/bsp/l4t-gcc/aarch64--glibc--stable-2022.08-1'
+local esp = (vim.env.IDF_TOOLS_PATH or vim.env.HOME .. '/.espressif') .. '/tools'
 
 return {
   cmd = {
@@ -8,6 +9,8 @@ return {
       '/usr/bin/g++',
       tc .. '/bin/aarch64*-gcc',
       tc .. '/bin/aarch64*-g++',
+      esp .. '/**/bin/*-elf-gcc',
+      esp .. '/**/bin/*-elf-g++',
     }, ','),
   },
 }
